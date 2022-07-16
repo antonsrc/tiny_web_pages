5de2067808c4d0af24457a4d76ce0a2976bf4696
AntonSource <anton.source@gmail.com>
2022-07-16 22:20:21 +0700

    add labelframe

--- gui.py
+++ gui.py
@@ -4,10 +4,12 @@ from ctypes import windll
 
 def show_text(*args):
     lbl_title_show_var.set('header: ' + str(entry_title.get()))
+    lbl_maintxt_show_var.set('content: ' + str(main_text.get(1.0, END)))
 
 def clear_all():
     entry_title.delete(0, END)
-    lbl_title_show_var.set('header: ')
+    lbl_title_show_var.set('')
+    lbl_maintxt_show_var.set('')
     main_text.delete(1.0, END)
 
 root = Tk()
@@ -16,33 +18,42 @@ root.geometry('500x450+400+400')
 windll.shcore.SetProcessDpiAwareness(1)
 root.tk.call('tk', 'scaling', 2)
 mainframe = ttk.Frame(root, padding="3 3 3 3")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S), padx=3, pady=3)
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-
-
 ttk.Label(mainframe, text='Title:').grid(column=1, row=1, sticky=W)
 
-entry_title = ttk.Entry(mainframe, width=15, textvariable=StringVar())
-entry_title.grid(column=1, row=2, sticky=W)
+entry_title = ttk.Entry(mainframe, textvariable=StringVar())
+entry_title.grid(column=1, row=2, sticky=(E, W))
 entry_title.focus()
 
+lf_title = ttk.Labelframe(mainframe, text='header:')
+lf_title.grid(column=3, row=2, sticky=(W, N))
 lbl_title_show_var = StringVar()
 lbl_title_show_var.set('')
-lbl_title_show = ttk.Label(mainframe, textvariable=lbl_title_show_var)
-lbl_title_show.grid(column=1, row=3, sticky=W)
+lbl_title_show = ttk.Label(lf_title, textvariable=lbl_title_show_var)
+lbl_title_show.grid(sticky=(W, N, S, E))
 
-main_text = Text(mainframe, width=25, height=10)
-main_text.grid(column=1, row=4, sticky=W)
+ttk.Label(mainframe, text='Main content:').grid(column=1, row=1, sticky=W)
+
+main_text = Text(mainframe, width=40, height=10)
+main_text.grid(column=1, row=4, sticky=(W, N, E))
 scrollbar = ttk.Scrollbar(mainframe, orient=VERTICAL, command=main_text.yview)
-scrollbar.grid(column=2, row=4, sticky=(N,S))
+scrollbar.grid(column=2, row=4, sticky=(N, S, W))
 main_text['yscrollcommand'] = scrollbar.set
 
+lf_maintxt = ttk.Labelframe(mainframe, text='content:')
+lf_maintxt.grid(column=3, row=4, sticky=(W, N, E), rowspan=5)
+lbl_maintxt_show_var = StringVar()
+lbl_maintxt_show_var.set('')
+lbl_maintxt_show = ttk.Label(lf_maintxt, textvariable=lbl_maintxt_show_var)
+lbl_maintxt_show.grid(sticky=(W, N, S, E))
 
-ttk.Button(mainframe, text='Show text', command=show_text).grid(column=1, row=5, sticky=W)
-root.bind("<Return>", show_text)
+btn_show_text = ttk.Button(mainframe, text='Show text', command=show_text)
+btn_show_text.grid(column=1, row=5, sticky=W)
 
-ttk.Button(mainframe, text='Clear', command=clear_all).grid(column=1, row=6, sticky=W)
+btn_clear = ttk.Button(mainframe, text='Clear', command=clear_all)
+btn_clear.grid(column=1, row=6, sticky=W)
 
 root.mainloop()
